6527668870ec4f8168b97b64f8568cc1e191177b
James Moore <james@knowthen.com>
2018-02-28 17:28:42 -0800

    compose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const R = require('ramda');
+const table = require('text-table');
 const cities = require('./cities.json');
 const percentile = require('./percentile');
 
@@ -10,7 +11,7 @@ const updateTemperature = R.curry((convertFn, city) => {
   return R.merge(city, { temp });
 });
 
-const updatedCities = R.map(updateTemperature(KtoF), cities);
+// const updatedCities = R.map(updateTemperature(KtoF), cities);
 
 // console.log(updatedCities);
 
@@ -23,8 +24,8 @@ const totalCostReducer = (acc, city) => {
   return acc + cost;
 }
 
-const totalCost = R.reduce(totalCostReducer, 0, updatedCities);
-const cityCount = R.length(updatedCities);
+const totalCost = R.reduce(totalCostReducer, 0, cities);
+const cityCount = R.length(cities);
 // console.log(totalCost / cityCount);
 
 const groupByPropReducer = (acc, city) => {
@@ -35,7 +36,7 @@ const groupByPropReducer = (acc, city) => {
   });
 }
 
-const groupedByProp = R.reduce(groupByPropReducer, {}, updatedCities);
+const groupedByProp = R.reduce(groupByPropReducer, {}, cities);
 
 // console.log(groupedByProp);
 
@@ -52,7 +53,7 @@ const calcScore = city => {
   return R.merge(city, { score });
 }
 
-const scoredCities = R.map(calcScore, updatedCities);
+// const scoredCities = R.map(calcScore, updatedCities);
 
 // console.log(scoredCities);
 
@@ -61,6 +62,44 @@ const filterByWeather = city => {
   return temp > 68 && temp < 85 && humidity > 30 && humidity < 70;
 }
 
-const filteredCities = R.filter(filterByWeather, scoredCities);
+// const filteredCities = R.filter(filterByWeather, scoredCities);
 
-console.log(R.length(filteredCities));
+// console.log(R.length(filteredCities));
+
+// const sortedCities = R.sortWith(
+//   [R.descend(city => city.score)],
+//   filteredCities,
+// );
+
+// console.log(sortedCities);
+
+// const top10 = R.take(10, sortedCities);
+
+// console.log(top10);
+// console.log(R.length(top10));
+
+const cityToArray = city => {
+  const { name, country, score, cost, temp, internetSpeed } = city;
+  return [name, country, score, cost, temp, internetSpeed];
+};
+const interestingProps = [
+  'Name',
+  'Country',
+  'Score',
+  'Cost',
+  'Temp',
+  'Internet',
+];
+
+const topCities = R.pipe(
+  R.map(updateTemperature(KtoF)),
+  R.filter(filterByWeather),
+  R.map(calcScore),
+  R.sortWith([R.descend(city => city.score)]),
+  R.take(10),
+  R.map(cityToArray),
+  R.prepend(interestingProps),
+  table,
+)(cities);
+
+console.log(topCities);
